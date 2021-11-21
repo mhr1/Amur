61c9ec6513062dc3dc227464ce88442872b0be57
Michael C. Riley <6481421+mhr1@users.noreply.github.com>
2021-11-21 20:42:13 +0000

    Update prep.py

--- prep.py
+++ prep.py
@@ -1,4 +1,5 @@
 
+
 def make_db(db_list, filename):
     file = open(filename, "r") 
 
@@ -12,6 +13,25 @@ def make_db(db_list, filename):
     file.close()
 
 audusd = []
+eurchf = []
+eurgbp = []
+eurjpy = []
+eurusd = []
+gbpjpy = []
+gbpusd = []
+usdcad = []
+usdchf = []
+usdjpy = []
 
 make_db(audusd, "./fx_daily/AUDUSD1440.csv") 
+make_db(eurchf, "./fx_daily/EURCHF1440.csv") 
+make_db(eurgbp, "./fx_daily/EURGBP1440.csv") 
+make_db(eurjpy, "./fx_daily/EURJPY1440.csv") 
+make_db(eurusd, "./fx_daily/EURUSD1440.csv") 
+make_db(gbpjpy, "./fx_daily/GBPJPY1440.csv") 
+make_db(gbpusd, "./fx_daily/GBPUSD1440.csv") 
+make_db(usdcad, "./fx_daily/USDCAD1440.csv") 
+make_db(usdchf, "./fx_daily/USDCHF1440.csv") 
+make_db(usdjpy, "./fx_daily/USDJPY1440.csv") 
 print(audusd[435][1])
+print(usdjpy[435][1])
